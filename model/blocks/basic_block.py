63911e01647318d7cae1d751585f8dc96e2a8009
bozhouchen <bozhouchen@hit.edu.cn>
2022-06-29 22:14:35 +0800

    dev

---
+++ model/blocks/basic_block.py
@@ -0,0 +1,88 @@
+from torch import nn
+import torch.nn.functional as F
+import torch
+
+
+class ResidualBlock(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(ResidualBlock, self).__init__()
+        hidden_planes = round(in_planes / self.expansion)
+        self.conv1 = nn.Conv2d(in_planes, hidden_planes,
+                               kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+
+        self.conv2 = nn.Conv2d(hidden_planes, hidden_planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False, groups=planes)
+        self.bn2 = nn.BatchNorm2d(hidden_planes)
+
+        self.conv3 = nn.Conv2d(hidden_planes, planes,
+                               kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class InvertedResidualBlock(nn.Module):
+    expansion = 6
+
+    def __init__(self, inplanes, planes, stride=1):
+        super(InvertedResidualBlock, self).__init__()
+        hidden_planes = round(inplanes * self.expansion)
+
+        # pw
+        self.conv1 = nn.Conv2d(inplanes, hidden_planes, kernel_size=1,
+                               stride=1, padding=0, bias=False)
+        self.bn1 = nn.BatchNorm2d(hidden_planes)
+        # dw
+        self.conv2 = nn.Conv2d(hidden_planes, hidden_planes, kernel_size=3,
+                               stride=stride, padding=1, groups=hidden_planes, bias=False)
+        self.bn2 = nn.BatchNorm2d(hidden_planes)
+        # pw-linear
+        self.conv3 = nn.Conv2d(hidden_planes, planes, kernel_size=1,
+                               stride=1, padding=0, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or inplanes != hidden_planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(inplanes, planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes)
+            )
+
+    def forward(self, x):
+        out = F.relu6(self.bn1(self.conv1(x)), inplace=True)
+        out = F.relu6(self.bn2(self.conv2(out)), inplace=True)
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+if __name__ == '__main__':
+    # train_loader, test_loader, input_channel, inputdim, nclass = Data().get(CIFAR10)
+    # model = IRFBlock(in_channels=input_channel, out_channels=3)
+    model = nn.Conv2d(3, 3, kernel_size=2,
+                      stride=1, padding=0, bias=False, groups=1)
+    for name, param in model.named_parameters():
+        print(name, param.size(), param.sum(3).sum(2).sum(1))
+    input = torch.ones(3, 4, 4)
+    print(input.size())
+    out = model(input)
+    print(out.size(), out)
--- model/nas_model/soft_step.py
+++ model/nas_model/soft_step.py
@@ -1,6 +1,37 @@
+from turtle import forward
 import torch.nn as nn
+from const import CIFAR10, MNIST
+from model.blocks.basic_block import ResidualBlock, InvertedResidualBlock
+from utils import Data
+import torch
 
 
 class SoftStep(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, input_channel, ndim, num_classes, block=ResidualBlock) -> None:
+        super(SoftStep, self).__init__()
+        block_input_channel = 32
+        self.conv_in = nn.Sequential(
+            nn.Conv2d(input_channel, out_channels=block_input_channel,
+                      kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(block_input_channel),
+            nn.ReLU6(inplace=True)
+        )
+
+        return
+
+    def forward(self, x):
+        out = self.conv_in(x)
+        return out
+
+    def test_model(self):
+
+        return
+
+
+if __name__ == '__main__':
+    train_loader, test_loader, input_channel, inputdim, nclass = Data().get(CIFAR10)
+    print(input_channel, inputdim, nclass)
+    # model = IRFBlock(in_channels=input_channel, out_channels=3)
+    model = SoftStep(input_channel, inputdim, nclass)
+    x = torch.rand(3, 32, 32)
+    preds = model(x)
