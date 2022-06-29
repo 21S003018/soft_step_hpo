99a65660d6d79241007200b572dee34236e53499
bozhouchen <bozhouchen@hit.edu.cn>
2022-06-29 16:23:41 +0800

    .

---
+++ trainers.py
@@ -0,0 +1,108 @@
+from enum import auto
+import json
+import pickle
+import copy
+import torch
+from torch.optim.adam import Adam
+from torch.optim.adamax import Adamax
+from const import *
+import torch.nn.functional as F
+import warnings
+from sklearn.metrics import precision_recall_fscore_support as metrics
+import numpy as np
+from utils import Data, num_image
+from time import time
+warnings.filterwarnings("ignore")
+
+
+class CNNTrainer():
+    """
+    specify for a dataset and a model
+    """
+
+    def __init__(self, model_name, dataset) -> None:
+        # init data
+        self.dataset = dataset
+        self.train_loader, self.test_loader, input_channel, inputdim, nclass = Data().get(dataset)
+        self.num_image = num_image(self.train_loader)  # get num of image
+        # init model
+        self.model_name = model_name
+        self.model = eval(self.model_name)(input_channel, inputdim, nclass)
+        self.save_model_path = f"ckpt/{self.model_name}_{self.dataset}"
+        pass
+
+    def train(self):
+        self.model.reset_parameters()
+        self.optimizer = torch.optim.SGD(
+            self.model.parameters(), lr=0.1, momentum=P_MOMENTUM)
+        lr_schedular = torch.optim.lr_scheduler.MultiStepLR(
+            self.optimizer, milestones=[EPOCHS * 0.5, EPOCHS * 0.75], gamma=0.1)
+        opt_accu = -1
+        for i in range(EPOCHS):
+            self.model.train()
+            loss_sum = 0
+            st_time = time()
+            for imgs, label in self.train_loader:
+                if torch.cuda.is_available():
+                    imgs = imgs.cuda()
+                    label = label.cuda()
+                preds = self.model(imgs)
+                loss = F.cross_entropy(preds, label)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                loss_sum += loss.item() * len(imgs)/self.num_image
+            # eval
+            val_accu, _, _, _, val_loss = self.val()
+            if val_accu > opt_accu:
+                self.save_model()
+            print(
+                f"Epoch~{i+1}->train_loss:{round(loss_sum,4)}, val_loss:{round(val_loss, 4)}, val_accu:{round(val_accu, 4)}, time:{round(time()-st_time,4)}")
+            lr_schedular.step()
+        return
+
+    def val(self):
+        self.model.eval()
+        ncorrect = 0
+        nsample = 0
+        valloss = 0
+        preds = []
+        Y = []
+        for imgs, label in self.test_loader:
+            if torch.cuda.is_available():
+                imgs = imgs.cuda()
+                label = label.cuda()
+            tmp_pred = self.model(imgs)
+            tmp = tmp_pred.detach().cpu().numpy()
+            preds.extend([np.argmax(tmp[i]) for i in range(len(tmp))])
+            Y.extend(label.detach().cpu().numpy())
+            ncorrect += torch.sum(tmp_pred.max(1)[1].eq(label).double())
+            nsample += len(label)
+            loss = F.cross_entropy(tmp_pred, label)
+            valloss += loss.item() * len(imgs)
+        p, r, f1, _ = metrics(preds, Y)
+        valloss = valloss/nsample
+        return float((ncorrect/nsample).cpu()), p, r, f1, valloss
+
+    def get_metrics(self):
+        self.load_model()
+        self.model.eval()
+        ncorrect = 0
+        nsample = 0
+        for imgs, label in self.test_loader:
+            if torch.cuda.is_available():
+                imgs = imgs.cuda()
+                label = label.cuda()
+            preds = self.model(imgs)
+            ncorrect += torch.sum(preds.max(1)[1].eq(label).double())
+            nsample += len(label)
+        return float((ncorrect/nsample).cpu())
+
+    def save_model(self):
+        torch.save(self.model.state_dict(), self.save_model_path)
+        return
+
+    def load_model(self):
+        state_dict = torch.load(self.save_model_path)
+        self.model.load_state_dict(state_dict)
+        return
---
+++ utils.py
@@ -0,0 +1,137 @@
+from zmq import device
+from const import *
+import sklearn.preprocessing as sp
+from sklearn.model_selection import train_test_split
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import numpy as np
+import pandas as pd
+import torch
+import pickle
+
+
+class Data():
+    def __init__(self) -> None:
+        self.datasets = DATASETS
+        if torch.cuda.is_available():
+            self.device = "cuda"
+        else:
+            self.device = "cpu"
+        return
+
+    def load_cifar10(self):
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(CIFAR10MEAN, CIFAR10STD),
+        ])
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(CIFAR10MEAN, CIFAR10STD),
+        ])
+        data_root_path = "data/"
+        train_dataset = datasets.CIFAR10(root=data_root_path, train=True,
+                                         transform=train_transform,
+                                         download=True)
+        test_dataset = datasets.CIFAR10(root=data_root_path, train=False,
+                                        transform=test_transform, download=True)
+        train_loader = DataLoader(dataset=train_dataset,
+                                  batch_size=NAME2BATCHSIZE[CIFAR10], shuffle=True,
+                                  num_workers=4,
+                                  )
+        test_loader = DataLoader(dataset=test_dataset,
+                                 batch_size=BATCHSIZE, shuffle=True,
+                                 )
+        return train_loader, test_loader, 3, 32, 10
+
+    def load_cifar100(self):
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(CIFAR100MEAN, CIFAR100STD),
+        ])
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(CIFAR100MEAN, CIFAR100STD),
+        ])
+        data_root_path = "data/"
+        train_dataset = datasets.CIFAR100(root=data_root_path, train=True,
+                                          transform=train_transform, download=True)
+        test_dataset = datasets.CIFAR100(root=data_root_path, train=False,
+                                         transform=test_transform, download=True)
+        train_loader = DataLoader(dataset=train_dataset,
+                                  batch_size=NAME2BATCHSIZE[CIFAR100], shuffle=True,
+                                  num_workers=4,
+                                  )
+        test_loader = DataLoader(dataset=test_dataset,
+                                 batch_size=BATCHSIZE, shuffle=True,
+                                 )
+        return train_loader, test_loader, 3, 32, 100
+
+    def load_mnist(self):
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(28, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(MNISTMEAN, MNISTSTD),
+        ])
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(MNISTMEAN, MNISTSTD),
+        ])
+        data_root_path = "data/"
+        train_dataset = datasets.MNIST(root=data_root_path, train=True,
+                                       transform=train_transform, download=True)
+        test_dataset = datasets.MNIST(root=data_root_path, train=False,
+                                      transform=test_transform, download=True)
+        train_loader = DataLoader(dataset=train_dataset,
+                                  batch_size=NAME2BATCHSIZE[MNIST], shuffle=True,
+                                  num_workers=4,
+                                  )
+        test_loader = DataLoader(dataset=test_dataset,
+                                 batch_size=BATCHSIZE, shuffle=True,
+                                 )
+        return train_loader, test_loader, 1, 28, 10
+
+    def load_svhn(self):
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(SVHNMEAN, SVHNSTD),
+        ])
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(SVHNMEAN, SVHNSTD),
+        ])
+        data_root_path = "data/SVHN/"
+        train_dataset = datasets.SVHN(root=data_root_path, split="train",
+                                      transform=train_transform, download=True)
+        test_dataset = datasets.SVHN(root=data_root_path, split="test",
+                                     transform=test_transform, download=True)
+        train_loader = DataLoader(dataset=train_dataset,
+                                  batch_size=NAME2BATCHSIZE[SVHN], shuffle=True,
+                                  num_workers=4)
+        test_loader = DataLoader(dataset=test_dataset,
+                                 batch_size=BATCHSIZE, shuffle=True)
+        return train_loader, test_loader, 3, 32, 10
+
+    def get(self, dataset):
+        if dataset == MNIST:
+            return self.load_mnist()
+        if dataset == SVHN:
+            return self.load_svhn()
+        if dataset == CIFAR10:
+            return self.load_cifar10()
+        if dataset == CIFAR100:
+            return self.load_cifar100()
+        return None
+
+
+def num_image(loader):
+    res = 0
+    for _, label in loader:
+        res += len(label)
+    return res
