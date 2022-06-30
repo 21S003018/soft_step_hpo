07a6517465acff9833d384d0834209a85d2cc4a3
bozhouchen <bozhouchen@hit.edu.cn>
2022-06-30 09:37:13 +0800

    .

--- model/nas_model/soft_step.py
+++ model/nas_model/soft_step.py
@@ -2,13 +2,21 @@ from turtle import forward
 import torch.nn as nn
 from const import CIFAR10, MNIST
 from model.blocks.basic_block import ResidualBlock, InvertedResidualBlock
-from utils import Data
-import torch
 
 
 class SoftStep(nn.Module):
     def __init__(self, input_channel, ndim, num_classes, block=ResidualBlock) -> None:
         super(SoftStep, self).__init__()
+        self.cfgs = [
+            # t, c, n, s
+            [1,  16, 1, 1],
+            [6,  24, 2, 2],
+            [6,  32, 3, 2],
+            [6,  64, 4, 2],
+            [6,  96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
         block_input_channel = 32
         self.conv_in = nn.Sequential(
             nn.Conv2d(input_channel, out_channels=block_input_channel,
@@ -16,6 +24,13 @@ class SoftStep(nn.Module):
             nn.BatchNorm2d(block_input_channel),
             nn.ReLU6(inplace=True)
         )
+        layers = []
+        for t, c, n, s in self.cfgs:
+            output_channel = c
+            for i in range(n):
+                layers.append(
+                    block(input_channel, output_channel, s if i == 0 else 1, t))
+                input_channel = output_channel
 
         return
 
@@ -29,9 +44,10 @@ class SoftStep(nn.Module):
 
 
 if __name__ == '__main__':
-    train_loader, test_loader, input_channel, inputdim, nclass = Data().get(CIFAR10)
-    print(input_channel, inputdim, nclass)
-    # model = IRFBlock(in_channels=input_channel, out_channels=3)
-    model = SoftStep(input_channel, inputdim, nclass)
-    x = torch.rand(3, 32, 32)
-    preds = model(x)
+    # train_loader, test_loader, input_channel, inputdim, nclass = Data().get(CIFAR10)
+    # print(input_channel, inputdim, nclass)
+    # # model = IRFBlock(in_channels=input_channel, out_channels=3)
+    # model = SoftStep(input_channel, inputdim, nclass)
+    # x = torch.rand(3, 32, 32)
+    # preds = model(x)
+    pass
