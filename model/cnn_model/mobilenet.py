9bafafc997a9de5780642356dfe73bbdfa561a09
bozhouchen <bozhouchen@hit.edu.cn>
2022-11-01 13:45:02 +0800

    .

--- model/cnn_model/mobilenet.py
+++ model/cnn_model/mobilenet.py
@@ -3,125 +3,104 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-# class LinearBottleNeck(nn.Module):
+class LinearBottleNeck(nn.Module):
 
-#     def __init__(self, in_channels, out_channels, stride, t=6):
-#         super().__init__()
+    def __init__(self, in_channels, out_channels, stride, t=6):
+        super().__init__()
 
-#         self.residual = nn.Sequential(
-#             nn.Conv2d(in_channels, in_channels * t, 1, bias=False),
-#             nn.BatchNorm2d(in_channels * t),
-#             nn.ReLU6(inplace=True),
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels * t, 1, bias=False),
+            nn.BatchNorm2d(in_channels * t),
+            nn.ReLU6(inplace=True),
 
-#             nn.Conv2d(in_channels * t, in_channels * t, 3,
-#                       stride=stride, padding=1, groups=in_channels * t, bias=False),
-#             nn.BatchNorm2d(in_channels * t),
-#             nn.ReLU6(inplace=True),
+            nn.Conv2d(in_channels * t, in_channels * t, 3,
+                      stride=stride, padding=1, groups=in_channels * t, bias=False),
+            nn.BatchNorm2d(in_channels * t),
+            nn.ReLU6(inplace=True),
 
-#             nn.Conv2d(in_channels * t, out_channels, 1, bias=False),
-#             nn.BatchNorm2d(out_channels)
-#         )
+            nn.Conv2d(in_channels * t, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
 
-#         self.stride = stride
-#         self.in_channels = in_channels
-#         self.out_channels = out_channels
+        self.stride = stride
+        self.in_channels = in_channels
+        self.out_channels = out_channels
 
-#     def forward(self, x):
+    def forward(self, x):
 
-#         residual = self.residual(x)
+        residual = self.residual(x)
 
-#         if self.stride == 1 and self.in_channels == self.out_channels:
-#             residual += x
+        if self.stride == 1 and self.in_channels == self.out_channels:
+            residual += x
 
-#         return residual
-
-
-# class MobileNetV2(nn.Module):
-
-#     def __init__(self, in_channel, ndim, num_classes=100):
-#         super().__init__()
-
-#         self.pre = nn.Sequential(
-# nn.Conv2d(3, 32, 1, padding=1),
-#             nn.BatchNorm2d(32),
-#             nn.ReLU6(inplace=True)
-#         )
-
-#         self.stage1 = LinearBottleNeck(32, 16, 1, 1)
-#         self.stage2 = self._make_stage(2, 16, 24, 2, 6)
-#         self.stage3 = self._make_stage(3, 24, 32, 2, 6)
-#         self.stage4 = self._make_stage(4, 32, 64, 2, 6)
-#         self.stage5 = self._make_stage(3, 64, 96, 1, 6)
-#         self.stage6 = self._make_stage(3, 96, 160, 1, 6)
-#         self.stage7 = LinearBottleNeck(160, 320, 1, 6)
-
-#         self.conv1 = nn.Sequential(
-#             nn.Conv2d(320, 1280, 1, bias=False),
-#             nn.BatchNorm2d(1280),
-#             nn.ReLU6(inplace=True)
-#         )
-
-#         self.conv2 = nn.Conv2d(1280, num_classes, 1, bias=False)
-
-#     def forward(self, x):
-#         x = self.pre(x)
-#         x = self.stage1(x)
-#         x = self.stage2(x)
-#         x = self.stage3(x)
-#         x = self.stage4(x)
-#         x = self.stage5(x)
-#         x = self.stage6(x)
-#         x = self.stage7(x)
-#         x = self.conv1(x)
-#         x = F.adaptive_avg_pool2d(x, 1)
-#         x = self.conv2(x)
-#         x = x.view(x.size(0), -1)
-
-#         return x
-
-#     def _make_stage(self, repeat, in_channels, out_channels, stride, t):
-
-#         layers = []
-#         layers.append(LinearBottleNeck(in_channels, out_channels, stride, t))
-
-#         while repeat - 1:
-#             layers.append(LinearBottleNeck(out_channels, out_channels, 1, t))
-#             repeat -= 1
-
-#         return nn.Sequential(*layers)
+        return residual
 
 
-"""
-Creates a MobileNetV2 Model as defined in:
-Mark Sandler, Andrew Howard, Menglong Zhu, Andrey Zhmoginov, Liang-Chieh Chen. (2018). 
-MobileNetV2: Inverted Residuals and Linear Bottlenecks
-arXiv preprint arXiv:1801.04381.
-import from https://github.com/tonylins/pytorch-mobilenet-v2
-"""
+class MobileNetV2(nn.Module):
 
-import torch.nn as nn
-import math
+    def __init__(self, in_channel, ndim, num_classes=100):
+        super().__init__()
+
+        self.pre = nn.Sequential(
+            nn.Conv2d(3, 32, 1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU6(inplace=True)
+        )
+
+        self.stage1 = LinearBottleNeck(32, 16, 1, 1)
+        self.stage2 = self._make_stage(2, 16, 24, 2, 6)
+        self.stage3 = self._make_stage(3, 24, 32, 2, 6)
+        self.stage4 = self._make_stage(4, 32, 64, 2, 6)
+        self.stage5 = self._make_stage(3, 64, 96, 1, 6)
+        self.stage6 = self._make_stage(3, 96, 160, 1, 6)
+        self.stage7 = LinearBottleNeck(160, 320, 1, 6)
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(320, 1280, 1, bias=False),
+            nn.BatchNorm2d(1280),
+            nn.ReLU6(inplace=True)
+        )
+
+        self.conv2 = nn.Conv2d(1280, num_classes, 1, bias=False)
+
+    def forward(self, x):
+        x = self.pre(x)
+        x = self.stage1(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+        x = self.stage5(x)
+        x = self.stage6(x)
+        x = self.stage7(x)
+        x = self.conv1(x)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+
+        return x
+
+    def _make_stage(self, repeat, in_channels, out_channels, stride, t):
+
+        layers = []
+        layers.append(LinearBottleNeck(in_channels, out_channels, stride, t))
+
+        while repeat - 1:
+            layers.append(LinearBottleNeck(out_channels, out_channels, 1, t))
+            repeat -= 1
+
+        return nn.Sequential(*layers)
 
 
-def _make_divisible(v, divisor, min_value=None):
-    """
-    This function is taken from the original tf repo.
-    It ensures that all layers have a channel number that is divisible by 8
-    It can be seen here:
-    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    :param v:
-    :param divisor:
-    :param min_value:
-    :return:
-    """
-    if min_value is None:
-        min_value = divisor
-    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
-    # Make sure that round down does not go down by more than 10%.
-    if new_v < 0.9 * v:
-        new_v += divisor
-    return new_v
+# """
+# Creates a MobileNetV2 Model as defined in:
+# Mark Sandler, Andrew Howard, Menglong Zhu, Andrey Zhmoginov, Liang-Chieh Chen. (2018). 
+# MobileNetV2: Inverted Residuals and Linear Bottlenecks
+# arXiv preprint arXiv:1801.04381.
+# import from https://github.com/tonylins/pytorch-mobilenet-v2
+# """
 
+# import torch.nn as nn
+# import math
 
 class InvertedResidual(nn.Module):
     def __init__(self, inp, oup, stride, expand_ratio):
@@ -183,7 +162,7 @@ class MobileNetV2(nn.Module):
         # building first layer
         input_channel = 32
         layers = [nn.Sequential(
-            nn.Conv2d(3, input_channel, 3, 1, 1, bias=False),
+            nn.Conv2d(3, input_channel, 1, 1, 1, bias=False),
             nn.BatchNorm2d(input_channel),
             nn.ReLU6(inplace=True)
         )]
