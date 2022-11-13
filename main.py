99b8ade8ced510de849eb97120d8842464c2d769
bozhouchen <bozhouchen@hit.edu.cn>
2022-11-13 17:04:02 +0800

    .

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ if __name__ == "__main__":
     # trainer.train()
     # trainer = CNNTrainer(MOBILENET, CIFAR10)
     # trainer.train()
-    trainer = EvalTrainer(CIFAR10, "config/search_space_linear.json")
-    trainer.train(save=True, epochs=int(EPOCHS/2))
+    # trainer = EvalTrainer(CIFAR10, "config/search_space_linear.json")
+    # trainer.train(save=True, epochs=int(EPOCHS/2))
 
     # trainer = SoftStepTrainer(
     #     SOFTSTEP, CIFAR10, path=LINEARSEARCHSPACE, opt_order=1)
     # trainer.train()
+    # trainer = SoftStepTrainer(
+    #     SOFTSTEP, CIFAR10, path=LINEARSEARCHSPACE, opt_order=2)
+    # trainer.train()
+    trainer = SoftStepTrainer(
+        SOFTSTEP, CIFAR100, path=LINEARSEARCHSPACE, opt_order=1)
+    trainer.train()
     # trainer = SoftStepTrainer(
     #     SOFTSTEP, CIFAR10, path=LINEARSEARCHSPACE, opt_order=2)
     # trainer.train()
