4983e4595ac784b42f96bb14ca05a2e9465e4f5d
1173710224 <bozhouchen@hit.edu.cn>
2022-11-23 20:26:35 +0800

    .

--- model/cnn_model/eval.py
+++ model/cnn_model/eval.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import json
 import torch.nn.functional as F
-from model.basicblocks import ResidualBlock, InvertedResidualBlock
+from model.basicblocks import BasicBlock, ResidualBlock, InvertedResidualBlock
 
 
 class Eval(nn.Module):
@@ -65,6 +65,46 @@ class BottleneckEval(nn.Module):
             in_planes = out_planes
             out_planes = c3
             blocks.append(block(in_planes, c1, c2, c3, k, s, t))
+        self.blocks = nn.Sequential(*blocks)
+        # post conv
+        input_channel = out_planes
+        output_channel = config["layer"]["conv_out"]
+        self.conv_out, self.bn_out = nn.Conv2d(
+            input_channel, output_channel, kernel_size=1, stride=1, padding=0, bias=False), nn.BatchNorm2d(output_channel)
+        # final
+        self.fc = nn.Linear(output_channel, num_classes)
+        return
+
+    def forward(self, x):
+        x = F.relu6(self.bn_in(self.conv_in(x)))
+        x = self.blocks(x)
+        x = F.relu6(self.bn_out(self.conv_out(x)))
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+class ShallowEval(nn.Module):
+    def __init__(self, input_channel, ndim, num_classes, path=None, block=BasicBlock) -> None:
+        super(ShallowEval, self).__init__()
+        with open(path, 'r') as f:
+            config = json.load(f)
+        assert config["type"] == "bottleneck"
+        # pre conv
+        input_channel = input_channel
+        output_channel = config["layer"]["conv_in"]
+        self.conv_in, self.bn_in = nn.Conv2d(
+            input_channel, output_channel, kernel_size=3, stride=1, padding=1, bias=False), nn.BatchNorm2d(output_channel)
+        # block
+        out_planes = output_channel
+        blocks = []
+        for block_config in config["block"]:
+            t, c1, k1, c2, k2, s = block_config['type'], block_config[
+                'c1'], block_config['k1'], block_config['c2'], block_config['k2'], block_config['s']
+            in_planes = out_planes
+            out_planes = c2
+            blocks.append(block(in_planes, c1, k1, c2, k2, s, t))
         self.blocks = nn.Sequential(*blocks)
         # post conv
         input_channel = out_planes
--- trainers.py
+++ trainers.py
@@ -10,8 +10,8 @@ from time import time
 from torchstat import stat
 from model.cnn_model.resnet import ResNet
 from model.cnn_model.mobilenet import MobileNetV2
-from model.cnn_model.eval import Eval, BottleneckEval
-from model.nas_model.softstep import SoftStep, BottleneckSoftStep
+from model.cnn_model.eval import Eval, BottleneckEval, ShallowEval
+from model.nas_model.softstep import SoftStep, BottleneckSoftStep, ShallowSoftStep
 warnings.filterwarnings("ignore")
 
 
@@ -280,16 +280,19 @@ if __name__ == "__main__":
     # print(stat(trainer.model, (3, 32, 32)))
     # model = BottleneckEval(
     # 3, 32, 100, path='config/search_space_bottleneck_eval.json')
+    model = ShallowEval(
+        3, 32, 100, path='config/search_space_shallow_eval.json')
     # model = BottleneckEval(
-    #     3, 32, 100, path='config/search_space_bottleneck_eval.json')
+    # 3, 32, 100, path='log/softstep/108_o1_cifar-100-python.json')
     # model = Eval(3, 32, 100, path='config/search_space_linear_eval.json')
     # model = Eval(
     #     3, 32, 100, path='log/softstep_linear_1e-5_expansion4/192_o1_cifar-100-python.json')
     # model = ResNet(3, 32, 100)
     # model = MobileNetV2(3, 32, 100)
-    # print(stat(model, (3, 32, 32)))
+    print(stat(model, (3, 32, 32)))
 
-    # model = SoftStep(3, 32, 100, path='config/search_space_linear.json')
+    # model = ShallowSoftStep(
+    #     3, 32, 100, path='config/search_space_shallow.json')
     # with open("test.json", "w") as f:
     #     json.dump(model.generate_config(True), f)
 
